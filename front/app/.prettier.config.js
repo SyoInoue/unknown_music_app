7b64742719868b7a07da35c05345c7f46ae35419
SyoInoue <funnyfacee7777@gmail.com>
2022-11-10 12:35:11 +0900

    eslint_tailwindcss_setting

---
+++ front/app/.prettier.config.js
@@ -0,0 +1,11 @@
+module.export = {
+  singleQuote: true,
+  semi: false,
+  jsxSingleQuote: true,
+  arrowParens: "always",
+  trailingComma: "all",
+  htmlWhitespaceSensitivity: "ignore",
+  printWidth: 80,
+  bracketSpacing: true,
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
